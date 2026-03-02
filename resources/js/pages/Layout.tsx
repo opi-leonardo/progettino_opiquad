af743fb80667fba26b985de5b65aa3bcaedbefcb
daniele de nuntiis <daniele.denuntiis@opiquad.it>
2026-03-02 09:16:04 +0100

    layout

--- resources/js/pages/Layout.tsx
+++ resources/js/pages/Layout.tsx
@@ -1,41 +1,49 @@
-import React from 'react';
-import { Link, usePage } from '@inertiajs/react';
+import React, { useEffect, useState } from 'react';
+import { router, usePage } from '@inertiajs/react';
 import { Layout as AntLayout, Menu } from 'antd';
 import Sider from 'antd/es/layout/Sider';
 import { Content } from 'antd/es/layout/layout';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { url } = usePage(); 
+  
+  const [currentSection, setCurrentSection] = useState<string>('users');
+  const { url } = usePage();
+
+  useEffect(() => {
+    if (url === '/')
+      router.visit('/users', { replace: true });
+    if (url.startsWith('/offices')) 
+      setCurrentSection('offices');
+  },[]);
 
   return (
-    <AntLayout style={{ minHeight: '100vh' }}>
-      <Sider
-        width={200}
-        style={{
-          background: '#001529',
-          color: '#fff',
-        }}
-      >
-        <div style={{ height: 64, margin: 16, color: '#fff', fontSize: 20 }}>
+    <AntLayout style={{ height: '100vh', overflow: 'hidden' }}>
+      <Sider width={200} style={{ background: '#001529', color: '#fff' }}>
+        <div style={{ height: 64, margin: 16, color: '#fff', fontSize: 20, padding:10 }}>
           My App
         </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          selectedKeys={[currentSection]}
           style={{ height: '100%', borderRight: 0 }}
-        >
-          <Menu.Item key="1">
-            <Link href="/users" style={{ color: '#fff' }}>Users</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link href="/offices" style={{ color: '#fff' }}>Offices</Link>
-          </Menu.Item>
-        </Menu>
+          items={[
+            {
+              key: 'users',
+              label: 'Users',
+              onClick: () => {router.get('/users'); setCurrentSection('users')},
+            },
+            {
+              key: 'offices',
+              label: 'Offices',
+              onClick: () => {router.get('/offices'); setCurrentSection('offices')},
+            },
+          ]}
+        />
       </Sider>
 
-      <AntLayout style={{ padding: '24px' }}>
-        <Content>
+      <AntLayout>
+        <Content key={url} style={{ overflowY: 'auto', padding: '20px' }}> 
           {children}
         </Content>
       </AntLayout>
