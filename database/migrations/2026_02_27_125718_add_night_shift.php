c4ff6a16eeb040962ea86217cdda0f6a3a0c5727
daniele de nuntiis <daniele.denuntiis@opiquad.it>
2026-03-02 08:59:15 +0100

    migrations

--- database/migrations/2026_02_27_125718_add_night_shift.php
+++ database/migrations/2026_02_27_125718_add_night_shift.php
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('is_enabled');
+        Schema::table('offices', function (Blueprint $table) {
+            $table->dropColumn('night_shift');
         });
     }
 };
